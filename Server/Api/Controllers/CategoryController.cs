ebde89fa6404302893161a5547064ed50ac03893
Bui Duc Trung <73098281+buiductrung29@users.noreply.github.com>
2023-03-25 22:31:35 +0700

    update

--- Server/Api/Controllers/CategoryController.cs
+++ Server/Api/Controllers/CategoryController.cs
@@ -1,6 +1,8 @@
 using Api.Models.DTOs;
 using ApplicationLayer;
 using AutoMapper;
+using CoreLayer.Entities;
+using InfrastructureLayer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -51,6 +53,22 @@ namespace Api.Controllers
             {
                 return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
             }
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetCategoryHierarchy()
+        {
+            var result = (await _unitOfWork._categoryRepository.GetAllAsync()).SelectMany(_ => _.)
+        }
+
+        async Task<IEnumerable<Category>> GetChild(Guid id)
+        {
+            var table = await _unitOfWork._categoryRepository.GetAllAsync();
+            return table.Where(x => x.CategoryId == id || x.ParentId == id)
+                        .Union(table.Where(x => x.ParentId == id)
+                                    .SelectMany(y => GetChild(y.CategoryId)));
         }
     }
 }
