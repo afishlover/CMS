ce17a64297de17110ab0f0862740ed3f4f9ef5c1
Bui Duc Trung <73098281+buiductrung29@users.noreply.github.com>
2023-03-26 15:09:07 +0700

    update

--- Server/Api/MapperProfiles/CreateResourceProfile.cs
+++ Server/Api/MapperProfiles/CreateResourceProfile.cs
@@ -1,6 +1,15 @@
-﻿namespace Api.MapperProfiles
+﻿using Api.Models.DTOs;
+using AutoMapper;
+using CoreLayer.Entities;
+
+namespace Api.MapperProfiles
 {
-    public class CreateResourceProfile
+    public class CreateResourceProfile : Profile
     {
+        public CreateResourceProfile()
+        {
+
+            CreateMap<Resource, CreateResourceDTO>();
+        }
     }
 }
